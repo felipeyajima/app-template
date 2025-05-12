674278e206d3a1a4b136ebfcabd8a9e1c888cf10
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2025-05-12 19:34:11 -0300

    Update app-test-release.tf

--- app-test-release.tf
+++ app-test-release.tf
@@ -2,6 +2,7 @@ resource "helm_release" "java_app" {
   name  = "java-app"
   chart = "./app-chart"
   namespace  = "app-test"
+  create_namespace = true
   set {
     name  = "image.tag"
     value = "latest"
