65ff8198283bb2c38830340d3868b47fee401f37
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2025-05-12 19:21:56 -0300

    Update app-test-release.tf

--- app-test-release.tf
+++ app-test-release.tf
@@ -1,5 +1,13 @@
 resource "helm_release" "java_app" {
   name  = "java-app"
-  chart = "${path.module}/charts/java-app"
-  ...
+  chart = "./app-chart"
+  namespace  = "app-test"
+  set {
+    name  = "image.tag"
+    value = "latest"
+  }
+  set {
+    name  = "image.repository"
+    value = "felipebatista765/restaurants"
+  }
 }
